50d740f85f2aaa1424b80d746dadd66ee7365a67
kanttanhed <plinio.salazar@yahoo.com.br>
2023-03-10 19:54:52 -0300

    adding Save and getByCPF Customer in Controller

--- mscustomer/mscustomer/src/main/java/com/github/kanttanhed/mscustomer/controller/CustomerController.java
+++ mscustomer/mscustomer/src/main/java/com/github/kanttanhed/mscustomer/controller/CustomerController.java
@@ -1,15 +1,48 @@
 package com.github.kanttanhed.mscustomer.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.kanttanhed.mscustomer.domain.dtos.CustomerDTO;
+import com.github.kanttanhed.mscustomer.domain.entity.Customer;
+import com.github.kanttanhed.mscustomer.domain.repository.CustomerRepository;
+import com.github.kanttanhed.mscustomer.domain.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/customer")
+@RequiredArgsConstructor
 public class CustomerController {
+    private final CustomerService customerService;
 
     @GetMapping
     public String status(){
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody CustomerDTO customerDTO){
+
+      var customer = customerDTO.toModel();
+      customerService.save(customer);
+      URI headerLocation = ServletUriComponentsBuilder
+              .fromCurrentRequest()
+              .query("cpf={cpf}")
+              .buildAndExpand(customer.getCpf())
+              .toUri();
+      return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf){
+        var cliente = customerService.getByCPF(cpf);
+        if(cliente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
     }
 }
