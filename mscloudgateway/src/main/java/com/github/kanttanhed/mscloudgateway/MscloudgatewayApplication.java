15576b6a8530b5aea4a83acdfb6e302435e2c149
plinio.salazar@yahoo.com.br <plinio.salazar@yahoo.com.br>
2023-06-07 15:27:01 -0300

    Implementing Gateway

--- mscloudgateway/src/main/java/com/github/kanttanhed/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/com/github/kanttanhed/mscloudgateway/MscloudgatewayApplication.java
@@ -2,12 +2,28 @@ package com.github.kanttanhed.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
+
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MscloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
 	}
-
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+				.route(r -> r.path("/clients/**").uri("lb://msclients"))
+				.route(r -> r.path("/cards/**").uri("lb://mscards"))
+				.route(r -> r.path("/credit-assessments/**").uri("lb://mscreditappraiser"))
+				.build();
+	}
 }
